b317ffbb1574305e8818b76b6a4f4e1fe44ca19e
kage1414 <kylejohnson92294@gmail.com>
2021-05-13 13:37:53 -0400

    Working on rendering proxy data to dom

--- server/index.js
+++ server/index.js
@@ -2,13 +2,30 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 const PORT = 3000;
+const axios = require('axios');
 
-app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')), () => {
-  console.log('request');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.get('/courseContent', (req, res) => {
+
+  // console.log(req.headers);
+  axios.get('http://127.0.0.1:9800/content/item', {
+    params: req.query,
+    headers: req.headers
+  })
+    .then((response) => {
+      // console.log(response.dat);
+      res.send(response.data);
+    });
+  // res.send('sent');
+
 });
-
-// app.get('/', express.static(path.join(__dirname, 'client', 'dist')))
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT);
