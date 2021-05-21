d39c068212a187d3dc2e29e59f2fffde655ca531
kage1414 <kylejohnson92294@gmail.com>
2021-05-20 23:54:12 -0400

    Deployable app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const cors = require('cors');;
+// const cors = require('cors');
 const PORT = 3000;
 
 app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
