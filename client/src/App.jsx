bf957827d5d4c450d51892e9f9388affc4439870
kage1414 <kylejohnson92294@gmail.com>
2021-05-22 15:15:18 -0400

    Fix elements that didn't merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,31 +20,35 @@ class App extends React.Component {
     content.async = true;
     document.body.appendChild(content);
 
-    // const sidebar = document.createElement('script');
-    // sidebar.src = 'http://127.0.0.1:3004/index.js';
-    // sidebar.async = true;
-    // document.body.appendChild(sidebar);
+    const sidebar = document.createElement('script');
+    sidebar.src = 'http://127.0.0.1:3004/index.js';
+    sidebar.async = true;
+    document.body.appendChild(sidebar);
 
-    // const reviews = document.createElement('script');
-    // reviews.src = 'http://127.0.0.1:2712/reviewBundle.js';
-    // reviews.async = true;
-    // document.body.appendChild(reviews);
+    const reviews = document.createElement('script');
+    reviews.src = 'http://127.0.0.1:2712/reviewBundle.js';
+    reviews.async = true;
+    document.body.appendChild(reviews);
 
-    // const overview = document.createElement('script');
-    // overview.src = 'http://127.0.0.1:3100/overview.js';
-    // overview.async = true;
-    // document.body.appendChild(overview);
+    const overview = document.createElement('script');
+    overview.src = 'http://127.0.0.1:3000/overview.js';
+    overview.async = true;
+    document.body.appendChild(overview);
   }
 
   render() {
 
     return (
-      <Container>
-        <Service id="app"></Service>
-        <CourseContent id="content"></CourseContent>
-        <Sidebar id="sidebar"></Sidebar>
-        <Reviews id="reviews"></Reviews>
-      </Container>
+      <div>
+        <Container>
+          <Service id="overview"></Service>
+        </Container>
+        <Container>
+          <CourseContent id="content"></CourseContent>
+          <Sidebar id="sidebar"></Sidebar>
+          <Reviews id="reviews"></Reviews>
+        </Container>
+      </div>
     );
 
   }
