f3f89e6c7a9e46df3bba859815738fd5d4e9478e
kage1414 <kylejohnson92294@gmail.com>
2021-05-22 14:34:52 -0400

    Change port, add sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,10 +20,10 @@ class App extends React.Component {
     content.async = true;
     document.body.appendChild(content);
 
-    // const sidebar = document.createElement('script');
-    // sidebar.src = 'http://127.0.0.1:3004/index.js';
-    // sidebar.async = true;
-    // document.body.appendChild(sidebar);
+    const sidebar = document.createElement('script');
+    sidebar.src = 'http://127.0.0.1:3004/index.js';
+    sidebar.async = true;
+    document.body.appendChild(sidebar);
 
     // const reviews = document.createElement('script');
     // reviews.src = 'http://127.0.0.1:2712/reviewBundle.js';
