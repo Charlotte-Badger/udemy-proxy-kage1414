bc3c1a2d78afb497e9a908b83b7e82321a30335f
kage1414 <kylejohnson92294@gmail.com>
2021-05-22 15:40:04 -0400

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const cors = require('cors');
+const cors = require('cors');
 const PORT = 5000;
 
 app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT);
