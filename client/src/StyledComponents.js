64300d6d80de2d385c086c996cc774a4fd8a6994
kage1414 <kylejohnson92294@gmail.com>
2021-06-03 22:55:14 -0400

    remove sidebar style

--- client/src/StyledComponents.js
+++ client/src/StyledComponents.js
@@ -23,7 +23,6 @@ export const Sidebar = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   display: inline-block;
   position: fixed;
-  padding: 24px;
   top: 25px;
 `;
 
