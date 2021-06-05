cd177a84655f5b7d94a4fe787d7bede29f8feb73
kage1414 <kylejohnson92294@gmail.com>
2021-06-05 14:55:39 -0400

    Remove max-width from sidebar

--- client/src/StyledComponents.js
+++ client/src/StyledComponents.js
@@ -18,7 +18,6 @@ export const CourseContent = styled.div`
 
 export const Sidebar = styled.div`
   background-color: white;
-  max-width: 292px;
   border-radius: 4px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   display: inline-block;
